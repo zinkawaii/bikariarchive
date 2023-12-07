18dc9bbf05cada0318ad379d9ced656c377e3e05
KazariEX <1364035137@qq.com>
2023-12-07 21:19:08 +0800

    fix: 修复读取文章路径错误

--- server/utils/readArticle.ts
+++ server/utils/readArticle.ts
@@ -4,7 +4,7 @@ import Article from "~/utils/Article";
 export async function readArticle(art: Article): Promise<string> {
     return new Promise((resolve, reject) => {
         const { novel, volOrder, index } = art;
-        const path = `dist/novel/${novel[0].toUpperCase() + novel.slice(1)}.${volOrder}/${index}.txt`;
+        const path = r(`dist/novel/${novel[0].toUpperCase() + novel.slice(1)}.${volOrder}/${index}.txt`);
         fs.readFile(path, (err, data) => {
             if (err) {
                 reject(err);
