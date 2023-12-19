eca93e1d167e0b3248d59ea57adc1e39ad9aee34
KazariEX <1364035137@qq.com>
2023-12-20 03:06:10 +0800

    fix: 修复切换夜间模式时背景不应用视图转换

--- components/ui/Background.vue
+++ components/ui/Background.vue
@@ -1,27 +1,24 @@
 <script setup>
-    const settingStore = useSettingStore();
-    const type = ref();
-
-    settingStore.listen("dark-mode", () => {
-        type.value = settingStore.isDarkMode ?
-            "dark" : Zin.period === Zin.PERIOD_DAY ?
-            "day" :
-            "night";
-    }, {
-        viewTransition: true
-    });
+    const image = `url(/garden/background/bg_${
+        Zin.period === Zin.PERIOD_DAY ? "day" : "night"
+    }.webp)`;
 </script>
 
 <template>
-    <div class="z-background" :style="{ backgroundImage: `url(/garden/background/bg_${type}.webp)` }"></div>
+    <div class="z-background"></div>
 </template>
 
 <style lang="scss" scoped>
     .z-background {
         position: fixed;
         inset: 0;
+        background-image: v-bind("image");
         background-repeat: no-repeat;
         background-position: center;
         background-size: cover;
+
+        [z-dark] & {
+            background-image: url("/garden/background/bg_dark.webp");
+        }
     }
 </style>
