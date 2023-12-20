1290c883ee305f21262c81e2028ac343000bae97
KazariEX <1364035137@qq.com>
2023-12-20 21:04:56 +0800

    feat: 复现数据库定期备份

---
+++ server/plugins/mongodump.ts
@@ -0,0 +1,34 @@
+import dayjs from "dayjs";
+import schedule from "node-schedule";
+import { exec } from "child_process";
+
+export default defineNitroPlugin((app) => {
+    const config = useRuntimeConfig();
+    const { options } = config.mongoose;
+
+    const rule = new schedule.RecurrenceRule();
+
+    //每个星期天的23:33
+    rule.dayOfWeek = 0;
+    rule.hour = 23;
+    rule.minute = 33;
+    rule.tz = "UTC+8";
+
+    schedule.scheduleJob(rule, () => {
+        //获取当前日期
+        const date = dayjs.tz();
+
+        //备份路径
+        const path = r(`server/backup/${date.format("YYMMDD")}.archive`);
+
+        //运行命令
+        exec(`mongodump -u=${options.user} -p=${options.pass} -d=${options.dbName} --archive="${path}" --authenticationDatabase admin`, (err, stdout, stderr) => {
+            if (err) {
+                console.error(err);
+            }
+            else {
+                console.log("数据库备份成功！");
+            }
+        });
+    });
+});
