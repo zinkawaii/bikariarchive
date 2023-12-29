82f0ae5b9e1e53671ba9d05f06eacbe6fd3bb01a
KazariEX <1364035137@qq.com>
2023-12-30 05:52:21 +0800

    feat: 构建文章时元数据可实时写入

--- task/article.js
+++ task/article.js
@@ -4,6 +4,7 @@ import consola from "consola";
 import dayjs from "dayjs";
 import fm from "front-matter";
 import fs from "fs-extra";
+import lodash from "lodash";
 import * as glob from "glob";
 import * as marked from "marked";
 import * as path from "path";
@@ -41,12 +42,20 @@ const metaSrcDir = "../assets/json/Article.json";
 const metaOutDir = "../dist/json/Article.json";
 const mapOutDir = "../dist/json/Artmap.json";
 
+const jMeta = JSON.parse(fs.readFileSync(metaSrcDir));
+const jMap = {};
+
+const re = /^(.*?)\.(\d+)$/;
+
 (async () => {
     //从元信息和Front Matter生成全文和Article.json
     await timer("Meta-Info", generateMetaInfo)();
 
     //解析函数
-    const parse = timer("Markdown", simpleParse);
+    const parse = timer("Markdown", (pathname) => {
+        simpleParse(pathname);
+        outputFile();
+    });
 
     //监听
     chokidar.watch([`${srcDir}/**/*.md`])
@@ -56,97 +65,87 @@ const mapOutDir = "../dist/json/Artmap.json";
 //单文件解析
 function simpleParse(pathname) {
     const file = fs.readFileSync(pathname);
-    const data = fm(file.toString());
-    const result = marked.parse(data.body);
+    const { attributes, body } = fm(file.toString());
+    const result = marked.parse(body);
 
     //写入文件
     const outPath = pathname.replaceAll("\\", "/").replace(srcDir, outDir).replace(".md", ".txt");
     fs.outputFileSync(outPath, result);
 
-    //返回数据供进一步处理
-    return data;
+    const match = path.basename(path.resolve(pathname, "..")).match(re);
+    const novel = match[1].toLowerCase();
+    const volume = Number(match[2]);
+    const filename = path.basename(pathname, ".md");
+
+    //解析内容
+    const $ = cheerio.load(result);
+    const runtime = [...$("*")].some((e) => e?.name?.includes("-")) || void(0);
+    const wordCount = $("p").text().length;
+
+    //日期格式化
+    dateFormat(attributes, ["date", "updated", "refactored"]);
+
+    //生成映射
+    let index = "";
+    switch (jMeta[novel].type) {
+        case "novel":
+            index = filename.match(/(.*?)-(.*)/)[2];
+            break;
+        case "blog":
+            index = attributes.abbrlink;
+            break;
+    }
+    jMap[novel][index] = filename;
+
+    //写入数据
+    jMeta[novel].chapter[pathname] = {
+        index,
+        volume,
+        runtime,
+        wordCount,
+        ...attributes
+    };
 }
 
 function generateMetaInfo() {
-    const jFile = fs.readFileSync(metaSrcDir);
-    const jMeta = JSON.parse(jFile);
-
-    const re = /^(.*?)\.(\d+)$/;
     const filelist = glob.globSync(`${srcDir}/**/*.md`);
 
     //序号与文件名的映射表
-    const jMap = {};
     for (const key in jMeta) {
         jMap[key] = {};
 
         //在元数据中按顺序填入文章
-        jMeta[key].chapter = [];
+        jMeta[key].chapter = {};
         filelist
         .filter((pathname) => pathname.includes(key[0].toUpperCase() + key.slice(1)))
         .sort((a, b) => a.localeCompare(b))
         .forEach((pathname) => {
-            const filename = path.basename(pathname, ".md");
-            jMeta[key].chapter.push(filename);
+            jMeta[key].chapter[pathname] = {};
         });
     }
 
-    for (const pathname of filelist) {
-        const match = path.basename(path.resolve(pathname, "..")).match(re);
-        const novel = match[1].toLowerCase();
-        const volume = Number(match[2]);
-        const filename = path.basename(pathname, ".md");
-        const order = jMeta[novel].chapter.indexOf(filename);
+    filelist.forEach(simpleParse);
+}
 
-        //将正文写入文件并读取数据
-        const data = simpleParse(pathname);
-        const {
-            attributes,
-            body
-        } = data;
-
-        //解析内容
-        const result = marked.parse(body);
-        const $ = cheerio.load(result);
-        const runtime = [...$("*")].some((e) => e?.name?.includes("-")) || void(0);
-        const wordCount = $("p").text().length;
-
-        //日期格式化
-        dateFormat(attributes, ["date", "updated", "refactored"]);
-
-        //生成映射
-        let index = "";
-        switch (jMeta[novel].type) {
-            case "novel":
-                index = filename.match(/(.*?)-(.*)/)[2];
-                break;
-            case "blog":
-                index = attributes.abbrlink;
-                break;
-        }
-        jMap[novel][index] = filename;
-
-        //写入数据
-        jMeta[novel].chapter[order] = {
-            index,
-            volume,
-            runtime,
-            wordCount,
-            ...attributes
-        };
+//输出到文件
+function outputFile() {
+    const jNeta = lodash.cloneDeep(jMeta);
+    for (const key in jNeta) {
+        const jChapter = Object.values(jNeta[key].chapter);
+        jNeta[key].chapter = jChapter;
     }
 
-    //输出到文件
-    fs.outputFileSync(metaOutDir, JSON.stringify(jMeta));
+    fs.outputFileSync(metaOutDir, JSON.stringify(jNeta));
     fs.outputFileSync(mapOutDir, JSON.stringify(jMap));
+}
 
-    //日期格式化
-    function dateFormat(obj, keys) {
-        keys.forEach((key) => {
-            if (Reflect.has(obj, key)) {
-                obj[key] = dayjs(obj[key]).format("YYYY-MM-DD");
-            }
-        });
-    }
+//日期格式化
+function dateFormat(obj, keys) {
+    keys.forEach((key) => {
+        if (Reflect.has(obj, key)) {
+            obj[key] = dayjs(obj[key]).format("YYYY-MM-DD");
+        }
+    });
 }
 
 //统计执行时长
