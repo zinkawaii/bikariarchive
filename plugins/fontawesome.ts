a3e26cc8b50f4c902a07b23b55a4f34ebe23b3c6
KazariEX <1364035137@qq.com>
2024-01-02 20:40:11 +0800

    perf: 图标精准按需导入

--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -1,12 +1,43 @@
 import { library, config } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { fab } from "@fortawesome/free-brands-svg-icons";
-import { far } from "@fortawesome/free-regular-svg-icons";
-import { fas } from "@fortawesome/free-solid-svg-icons";
+import * as fab from "@fortawesome/free-brands-svg-icons";
+import * as fas from "@fortawesome/free-solid-svg-icons";
 
 config.autoAddCss = false;
 
-library.add(fab, far, fas);
+library.add(
+    fab.faBilibili,
+    fab.faGithub,
+    fab.faTwitter,
+    fas.faArrowUp,
+    fas.faArrowUpRightFromSquare,
+    fas.faArrowRight,
+    fas.faBullhorn,
+    fas.faBookOpen,
+    fas.faChevronLeft,
+    fas.faChevronRight,
+    fas.faChevronDown,
+    fas.faClockRotateLeft,
+    fas.faGear,
+    fas.faHouse,
+    fas.faLink,
+    fas.faMoon,
+    fas.faPaste,
+    fas.faPencil,
+    fas.faPersonPraying,
+    fas.faQuoteLeft,
+    fas.faQuoteRight,
+    fas.faRotateRight,
+    fas.faRss,
+    fas.faSearch,
+    fas.faSitemap,
+    fas.faSubway,
+    fas.faSun,
+    fas.faTrashCan,
+    fas.faToriiGate,
+    fas.faUser,
+    fas.faXmark
+);
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.component("fa-icon", FontAwesomeIcon);
