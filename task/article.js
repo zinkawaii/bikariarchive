a0b97305825dc2dea12e70a234a6334df8372564
KazariEX <1364035137@qq.com>
2024-01-21 04:24:01 +0800

    feat: 自定义 `ruby` 语法

--- task/article.js
+++ task/article.js
@@ -28,9 +28,34 @@ marked.use({
             return text.replaceAll("\n", "");
         }
     },
+    extensions: [
+        {
+            name: "ruby",
+            level: "inline",
+            start(src) {
+                return src.match(/\{/)?.index;
+            },
+            tokenizer(src, tokens) {
+                const rule = /^\{([^{\n]*?)\|([^}\n]*?)\}/;
+                const match = rule.exec(src);
+                if (match) {
+                    return {
+                        type: "ruby",
+                        raw: match[0],
+                        ruby: this.lexer.inlineTokens(match[1].trim()),
+                        rt: this.lexer.inlineTokens(match[2].trim())
+                    };
+                }
+            },
+            renderer(token) {
+                return `<ruby>${this.parser.parseInline(token.ruby)}<rt>${this.parser.parseInline(token.rt)}</rt></ruby>`;
+            },
+            childTokens: ["ruby", "rt"]
+        }
+    ],
     hooks: {
         postprocess(html) {
-            return html.replaceAll(/<br(\s*)\/>/g, "<p><br /></p>");
+            return html.replaceAll(/(?<=\n)<br(\s*)\/>/g, "<p><br /></p>");
         }
     }
 });
