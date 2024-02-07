4e134164138a6c59b33b9ff47f80970ca8ec0f49
KazariEX <1364035137@qq.com>
2024-02-07 11:49:44 +0800

    chore: 挂载备案号

--- components/ui/Footer.vue
+++ components/ui/Footer.vue
@@ -20,7 +20,7 @@
 <template>
     <footer class="z-footer">
         <p>茶馆已营业 {{ time.day }} 天 {{ time.hour }} 时 {{ time.minute }} 分 {{ time.second }} 秒</p>
-        <p>Copyright © 2022-2024 By KazariEX｜<nuxt-link href="https://icp.gov.moe/?keyword=20230904" target="_blank">萌ICP备20230904号</nuxt-link></p>
+        <p>Copyright © 2022-2024 By KazariEX｜<nuxt-link to="https://icp.gov.moe/?keyword=20230904" target="_blank">萌ICP备20230904号</nuxt-link>｜<nuxt-link to="https://beian.miit.gov.cn" target="_blank">浙ICP备2024064887号</nuxt-link></p>
     </footer>
 </template>
 
