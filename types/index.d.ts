55e37eaf7c1c40fd31fd9ce84f5a3079a66bda7a
KazariEX <1364035137@qq.com>
2024-03-07 14:30:49 +0800

    refactor: 为 `Article.json` 添加类型声明

---
+++ types/index.d.ts
@@ -0,0 +1,32 @@
+declare module "~/dist/json/Article.json" {
+    export default <{
+        [novel: string]: JNovel
+    }> {};
+
+    export interface JNovel {
+        author: string,
+        title: string,
+        type: "novel" | "blog",
+        tag: string[],
+        synopsis: string,
+        cover: string,
+        volumes: JVolume[],
+        chapters: jChapter[],
+        $seq: string[]
+    }
+
+    export interface JVolume {
+        title: string
+    }
+
+    export interface JChapter {
+        index: string,
+        volume: number,
+        title: string,
+        date?: string,
+        refactored?: string,
+        updated?: string,
+        runtime?: boolean,
+        wordCount: number
+    }
+}
