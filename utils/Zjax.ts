188e6ecd18d1a4f51baeb299edbbb514f6b0e9e0
KazariEX <1364035137@qq.com>
2024-03-07 21:35:10 +0800

    refactor: 简单重构 `Zjax`

--- utils/Zjax.ts
+++ utils/Zjax.ts
@@ -1,60 +1,58 @@
-type ZjaxOptions = Partial<{
-    url: string,
-    method: string,
-    data: any,
-    timeout: number
-}>;
-
-const Zjax = function({
-    url,
-    method,
-    data,
-    timeout
-}: ZjaxOptions) {
-    return Zjax[method]?.(url, { data, timeout });
-};
-
-Zjax.get = request("get");
-Zjax.post = request("post");
-Zjax.put = request("put");
-Zjax.delete = request("delete");
-
-function request(method: string) {
-    return async function(urlStr: string, {
-        body = {},
-        query = {},
-        timeout = 0
-    } = {}) {
-        const url = new URL(urlStr, location.origin);
-        const options: RequestInit = {
-            method,
-            headers: {
-                "Content-Type": "application/json"
-            }
-        };
-
-        //数据处理
-        for (const key in query) {
-            url.searchParams.append(key, query[key]);
-        }
-
-        if (Object.keys(body).length > 0) {
-            options.body = JSON.stringify(body);
-        }
-
-        //超时处理
-        if (timeout > 0) {
-            const controller = new AbortController();
-            options.signal = controller.signal;
-            setTimeout(() => controller.abort(), timeout);
-        }
-
-        const res = await fetch(url, options);
-        if (res.status >= 400) {
-            throw new Error(`The server responded with a status of ${res.status} ()`);
-        }
-        return res.json();
-    };
+interface ZjaxOptions {
+    body?: any,
+    query?: any,
+    timeout?: number
 }
 
-export default Zjax;
+//创建请求
+const createRequest = (method: string) => (url: string, options?: ZjaxOptions) => request(method, url, options);
+
+export default function Zjax(options: {
+    url: string,
+    method: string
+} & ZjaxOptions) {
+    return request(options.method, options.url, options);
+}
+
+Zjax.get = createRequest("get");
+Zjax.post = createRequest("post");
+Zjax.put = createRequest("put");
+Zjax.delete = createRequest("delete");
+
+//请求函数体
+async function request(method: string, urlStr: string, {
+    body = {},
+    query = {},
+    timeout = 0
+}: ZjaxOptions = {}) {
+    const url = new URL(urlStr, location.origin);
+    const options: RequestInit = {
+        method,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    };
+
+    //数据处理
+    for (const key in query) {
+        url.searchParams.append(key, query[key]);
+    }
+
+    if (Object.keys(body).length > 0) {
+        options.body = JSON.stringify(body);
+    }
+
+    //超时处理
+    if (timeout > 0) {
+        const controller = new AbortController();
+        options.signal = controller.signal;
+        setTimeout(() => controller.abort(), timeout);
+    }
+
+    //发送请求
+    const res = await fetch(url, options);
+    if (res.status >= 400) {
+        throw new Error(`The server responded with a status of ${res.status} ()`);
+    }
+    return res.json();
+}
