1acfc937adaf4516aa4bb97e8414e68ded8bc706
KazariEX <1364035137@qq.com>
2024-03-22 22:10:23 +0800

    chore: 简化 Styelint 配置

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,17 +1,3 @@
 import zin from "@zinkawaii/stylelint-config";
 
-export default {
-    extends: [
-        "stylelint-config-standard-scss",
-        "stylelint-config-html/vue"
-    ],
-    plugins: [
-        "stylelint-order",
-        "@stylistic/stylelint-plugin"
-    ],
-    rules: {
-        ...zin.order,
-        ...zin.standard,
-        ...zin.stylistic
-    }
-};
+export default zin();
