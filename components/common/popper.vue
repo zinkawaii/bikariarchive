ed185afcd21a3e7746a1f6f03486c531222fd222
KazariEX <1364035137@qq.com>
2024-05-07 10:08:21 +0800

    feat(mb-popper): 可定制方向与显示纯文本

--- components/common/popper.vue
+++ components/common/popper.vue
@@ -1,15 +1,19 @@
 <script lang="ts" setup>
-
+    withDefaults(defineProps<{
+        direction?: "top" | "right" | "bottom" | "left";
+        plaintext?: string;
+    }>(), {
+        direction: "bottom"
+    });
 </script>
 
 <template>
     <div class="mb-popper">
-        <div class="popper-wrapper">
-            <slot></slot>
-        </div>
-        <div v-if="$slots.floating" class="popper-outer">
+        <slot></slot>
+        <div v-if="$slots.floating || plaintext" class="popper-outer" :class="`is-${direction}`">
             <div class="popper-inner">
-                <slot name="floating"></slot>
+                <span v-if="plaintext" class="popper-plain">{{ plaintext }}</span>
+                <slot v-else name="floating"></slot>
             </div>
         </div>
     </div>
@@ -18,38 +22,81 @@
 <style lang="scss" scoped>
     .mb-popper {
         display: grid;
-        justify-items: center;
+        place-items: center;
         position: relative;
     }
 
     .popper-outer {
+        display: grid;
+        place-items: center;
         position: absolute;
         opacity: 0;
-        top: 100%;
-        padding-top: 8px;
-        transform-origin: top;
         transition: all 0.25s;
-        scale: 1 0.66;
         filter: drop-shadow(0 0 8px rgb(0 0 0 / 16%));
         pointer-events: none;
 
-        &::before {
-            content: "";
-            position: absolute;
-            inset: 4px 0;
-            width: 10px;
-            aspect-ratio: 1;
-            margin-inline: auto;
-            border: 1px solid var(--color-border-lighter);
-            background-color: var(--color-background);
-            clip-path: polygon(0 0, 0 100%, 100% 0);
-            rotate: 45deg;
+        &.is-top {
+            bottom: 100%;
+            padding-bottom: 8px;
+            transform-origin: bottom;
+            scale: 1 0.66;
+
+            &::before {
+                bottom: 4px;
+                rotate: -135deg;
+            }
+        }
+
+        &.is-right {
+            left: 100%;
+            padding-left: 8px;
+            transform-origin: left;
+            scale: 0.66 1;
+
+            &::before {
+                left: 4px;
+                rotate: -45deg;
+            }
+        }
+
+        &.is-bottom {
+            top: 100%;
+            padding-top: 8px;
+            transform-origin: top;
+            scale: 1 0.66;
+
+            &::before {
+                top: 4px;
+                rotate: 45deg;
+            }
+        }
+
+        &.is-left {
+            right: 100%;
+            padding-right: 8px;
+            transform-origin: right;
+            scale: 0.66 1;
+
+            &::before {
+                right: 4px;
+                rotate: 135deg;
+            }
         }
 
         :hover > & {
             opacity: 1;
             scale: 1;
             pointer-events: auto;
+        }
+
+        &::before {
+            content: "";
+            position: absolute;
+            width: 10px;
+            aspect-ratio: 1;
+            border: 1px solid var(--color-border-lighter);
+            background-color: var(--color-background);
+            clip-path: polygon(0 0, 0 100%, 100% 0);
         }
     }
 
@@ -58,5 +105,14 @@
         border: 1px solid var(--color-border-lighter);
         border-radius: 12px;
         background-color: var(--color-background);
+    }
+
+    .popper-plain {
+        padding-inline: 6px;
+        font-size: 14px;
+        line-height: 20px;
+        text-wrap: nowrap;
+        color: var(--color-text-secondary);
+        user-select: none;
     }
 </style>
