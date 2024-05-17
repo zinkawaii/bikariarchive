716b2d16d13f4dc399ca843aa4bfa513df85eaa4
KazariEX <1364035137@qq.com>
2024-05-17 15:01:02 +0800

    feat: 引入 `uqr` 动态渲染二维码

--- components/home/welcome.vue
+++ components/home/welcome.vue
@@ -1,8 +1,24 @@
+<script lang="ts" setup>
+    import { renderSVG } from "uqr";
+
+    const config = useRuntimeConfig();
+
+    const qrcode = ref("");
+    onMounted(() => {
+        qrcode.value = renderSVG(`https://${config.public.domain}`, {
+            border: 0,
+            ecc: "M",
+            whiteColor: "transparent",
+            blackColor: "var(--color-theme-dark)"
+        });
+    });
+</script>
+
 <template>
     <div class="content-table home-welcome">
         <p>
             <span class="content-h2 welcome-title">欢迎来到{{ $config.public.title }}！</span>
-            <i class="welcome-qrcode"></i><br />
+            <i class="welcome-qrcode" v-html="qrcode"></i><br />
             这里是我的个人网站，主要用于发布小说正文，所有文章均可在<plain-link :to="{ name: `catalogue` }">目录页</plain-link>索引并浏览。<plain-link :to="{ name: `details` }">情报页</plain-link>整理并展示了目前部分可以公开的设定。<plain-link :to="{ name: `search` }">检索页</plain-link>可在全文范围内对特定关键词进行检索。
         </p>
     </div>
@@ -17,13 +33,11 @@
     }
 
     .welcome-qrcode {
+        display: flex;
         float: right;
         width: 76px;
         aspect-ratio: 1;
         margin: $mt 0 0 4px;
-        background-color: var(--color-theme-dark);
-        mask-image: url("/garden/qrcode-site.svg");
-        mask-size: cover;
     }
 
     .welcome-title {
