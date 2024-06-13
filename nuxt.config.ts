3a5803baa2c361e26460e13a5f27de6c03e96002
KazariEX <1364035137@qq.com>
2024-06-13 11:18:24 +0800

    revert: 仍需顶级 `await` 支持

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -59,11 +59,21 @@ export default defineNuxtConfig({
     future: {
         compatibilityVersion: 4
     },
+    nitro: {
+        esbuild: {
+            options: {
+                target: "esnext"
+            }
+        }
+    },
     runtimeConfig: {
         ...serverConfig,
         public: clientConfig
     },
     vite: {
+        build: {
+            target: "esnext"
+        },
         css: {
             preprocessorOptions: {
                 scss: {
