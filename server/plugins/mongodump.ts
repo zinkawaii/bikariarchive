24c945af8d72adf06f9b354d8c9f56ca4e919681
KazariEX <1364035137@qq.com>
2024-07-21 01:37:12 +0800

    refactor: 更换定时任务包

--- server/plugins/mongodump.ts
+++ server/plugins/mongodump.ts
@@ -1,35 +1,32 @@
 import { exec } from "node:child_process";
+import { CronJob } from "cron";
 import consola from "consola";
 import dayjs from "dayjs";
-import schedule from "node-schedule";
 
 export default defineNitroPlugin(() => {
     const config = useRuntimeConfig();
     const { options } = config.mongoose;
 
-    const rule = new schedule.RecurrenceRule();
+    const job = CronJob.from({
+        cronTime: "0 33 23 * * 7",
+        timeZone: "UTC+8",
+        onTick() {
+            //获取当前日期
+            const date = dayjs.tz();
 
-    //每个星期天的23:33
-    rule.dayOfWeek = 0;
-    rule.hour = 23;
-    rule.minute = 33;
-    rule.tz = "UTC+8";
+            //备份路径
+            const path = r(`/server/backup/${date.format("YYMMDD")}.archive`);
 
-    schedule.scheduleJob(rule, () => {
-        //获取当前日期
-        const date = dayjs.tz();
-
-        //备份路径
-        const path = r(`/server/backup/${date.format("YYMMDD")}.archive`);
-
-        //运行命令
-        exec(`mongodump -u=${options.user} -p=${options.pass} -d=${options.dbName} --archive="${path}" --authenticationDatabase admin`, (err) => {
-            if (err) {
-                consola.error(err);
-            }
-            else {
-                consola.success("数据库备份成功！");
-            }
-        });
+            //运行命令
+            exec(`mongodump -u=${options.user} -p=${options.pass} -d=${options.dbName} --archive="${path}" --authenticationDatabase admin`, (err) => {
+                if (err) {
+                    consola.error(err);
+                }
+                else {
+                    consola.success("数据库备份成功！");
+                }
+            });
+        },
+        start: true
     });
 });
