0e6ea91a7c57bd621741beb3bbbf22e570d3a520
KazariEX <1364035137@qq.com>
2024-09-26 00:15:25 +0800

    refactor(shared): TS 化

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -4,8 +4,8 @@ import consola from "consola";
 export const isDev = process.env.NODE_ENV === "development";
 
 //统计执行时长
-export function timer(sign, func) {
-    return async function(...args) {
+export function timer<T extends unknown[]>(sign: string, func: (...args: T) => void) {
+    return async function(this: unknown, ...args: T) {
         const start = `${sign}:start`;
         const end = `${sign}:end`;
         const full = `${sign}:full`;
---
+++ packages/shared/tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+    entry: [
+        "./src/index.ts"
+    ],
+    format: [
+        "cjs",
+        "esm"
+    ],
+    clean: true,
+    dts: true
+});
