b90bde18f18d1635871e5210e7b7e9df9713d797
KazariEX <1364035137@qq.com>
2024-10-02 03:44:47 +0800

    feat(mb-input-number): 上下限支持

--- app/components/common/input-number.vue
+++ app/components/common/input-number.vue
@@ -1,9 +1,13 @@
 <script lang="ts" setup>
     const props = withDefaults(defineProps<{
         accuracy?: number;
+        min?: number;
+        max?: number;
         trim?: boolean;
     }>(), {
-        accuracy: 0
+        accuracy: 0,
+        min: -Infinity,
+        max: Infinity
     });
     const modelValue = defineModel<number>();
 
@@ -29,24 +33,37 @@
     //输入时
     function input() {
         const match = displayValue.value.match(NUMBER_REGEX);
-        if (match) {
-            const d = match[2]?.slice(0, props.accuracy + 1) || "";
-
-            oldDisplayValue = match[1] + d;
-            oldValue = Number(oldDisplayValue) || 0;
-            modelValue.value = oldValue;
+        if (!match) {
+            toggleValid(false);
+            return;
         }
-        toggleValid(!!match);
+
+        const d = match[2] || ".";
+        if (d.length > props.accuracy + 1) {
+            toggleValid(false);
+            return;
+        }
+
+        const str = match[1] + d;
+        const val = Number(str) || 0;
+        if (val < props.min || val > props.max) {
+            toggleValid(false);
+            return;
+        }
+
+        oldDisplayValue = str;
+        modelValue.value = oldValue = val;
+        toggleValid(true);
     }
 
     //失焦时
     function blur() {
         const match = oldDisplayValue.match(NUMBER_REGEX);
-        const i = Number(match[1]) || (match[1] === "-" ? "-0" : "0");
+        const i = Number(match[1]) || "0";
 
         let d = match[2] || ".";
         if (props.accuracy) {
-            d = d.padEnd(props.accuracy + 1, "0").slice(0, props.accuracy + 1);
+            d = d.padEnd(props.accuracy + 1, "0");
         }
         if (props.trim) {
             d = d.replace(/0+$/, "");
