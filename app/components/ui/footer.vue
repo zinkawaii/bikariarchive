8154bc5590e262341718d8ed953295ddef5d8640
KazariEX <1364035137@qq.com>
2024-10-05 22:08:40 +0800

    chore(footer): 移除走秒显示

--- app/components/ui/footer.vue
+++ app/components/ui/footer.vue
@@ -1,26 +1,14 @@
 <script lang="ts" setup>
-    const timerStore = useTimerStore();
-    const startTime = new Date("2022/09/30").getTime();
+    const startDate = new Date("2022/09/30");
+    const currentDate = new Date();
 
-    const time = computed(() => {
-        const sub = Math.floor((timerStore.now.getTime() - startTime) / 1000);
-        const d = Math.floor(sub / 60 / 60 / 24);
-        const h = Math.floor(sub / 60 / 60) - 24 * d;
-        const m = Math.floor(sub % 3600 / 60);
-        const s = Math.floor(sub % 60);
-        return {
-            day: d,
-            hour: h,
-            minute: m,
-            second: s
-        };
-    });
+    const day = Math.floor((currentDate.getTime() - startDate.getTime()) / 1000 / 60 / 60 / 24);
 </script>
 
 <template>
     <footer class="z-footer">
-        <p>茶馆已营业 {{ time.day }} 天 {{ time.hour }} 时 {{ time.minute }} 分 {{ time.second }} 秒</p>
-        <p>Copyright © 2022-2024 KazariEX｜<nuxt-link to="https://icp.gov.moe/?keyword=20230904" target="_blank">萌ICP备20230904号</nuxt-link>｜<nuxt-link to="https://beian.miit.gov.cn" target="_blank">浙ICP备2024064887号</nuxt-link></p>
+        <p>茶馆已营业 {{ day }} 天・おはびかり・</p>
+        <p>Copyright © 2022-{{ currentDate.getFullYear() }} KazariEX｜<nuxt-link to="https://icp.gov.moe/?keyword=20230904" target="_blank">萌ICP备20230904号</nuxt-link>｜<nuxt-link to="https://beian.miit.gov.cn" target="_blank">浙ICP备2024064887号</nuxt-link></p>
     </footer>
 </template>
 
