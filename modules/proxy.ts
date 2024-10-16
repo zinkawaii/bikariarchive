b165fc93c2d9a18f33aa1aa7c060b33846da9d23
KazariEX <1364035137@qq.com>
2024-10-16 16:58:27 +0800

    fix: 在构建阶段开启代理

--- modules/proxy.ts
+++ modules/proxy.ts
@@ -1,9 +1,7 @@
 import { defineNuxtModule } from "nuxt/kit";
 import { ProxyAgent, setGlobalDispatcher } from "undici";
 
-if (process.env.HTTPS_PROXY) {
-    const dispatcher = new ProxyAgent({ uri: new URL(process.env.HTTPS_PROXY).toString() });
-    setGlobalDispatcher(dispatcher);
-}
+const dispatcher = new ProxyAgent({ uri: new URL(process.env.HTTPS_PROXY).toString() });
+setGlobalDispatcher(dispatcher);
 
 export default defineNuxtModule({});
