ee3827ce28bb6aed41e014aae849beab3c7276c2
KazariEX <1364035137@qq.com>
2024-10-21 22:04:28 +0800

    feat: 提供更多 Apple 专用元标签

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,18 @@
 <script lang="ts" setup>
-    const config = useRuntimeConfig();
     const settingStore = useSettingStore();
+    const config = useRuntimeConfig();
+    const image = useImage();
 
     useHead({
         link: [
             { rel: "icon", href: "/garden/favicon.ico" },
+            { rel: "apple-touch-icon", href: image(config.public.avatar, { width: 180 }) },
             { rel: "alternate", type: "application/atom+xml", title: config.public.title, href: "/feed" }
+        ],
+        meta: [
+            { name: "apple-mobile-web-app-capable", content: "yes" },
+            { name: "apple-mobile-web-app-status-bar-style", content: "black-translucent" },
+            { name: "apple-mobile-web-app-title", content: config.public.title }
         ],
         titleTemplate: "%s %separator %siteName",
         templateParams: {
