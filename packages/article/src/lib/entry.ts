6e35047bcbae0cf859022f07f134d6b7670d83bf
KazariEX <1364035137@qq.com>
2024-10-21 21:15:44 +0800

    fix(entry): 正确处理无 `details` 的情况

--- packages/article/src/lib/entry.ts
+++ packages/article/src/lib/entry.ts
@@ -29,33 +29,8 @@ export default new Processor({
         const file = await fs.readFile(filename);
         const attributes = await parseEntry<JEntry>(file.toString());
 
-        const details: EntryDetail[] = [];
-        let detail: EntryDetail;
-
-        for (let i = 0; i < attributes.details.length; i++) {
-            const node = (attributes.details as unknown as Child[])[i];
-
-            if (node.type === "element" && node.tag === "h2") {
-                if (node.children.length !== 1) {
-                    continue;
-                }
-
-                const firstNode = node.children[0];
-                if (firstNode.type !== "text") {
-                    continue;
-                }
-
-                detail = {
-                    title: firstNode.value,
-                    content: []
-                };
-                details.push(detail);
-            }
-            else {
-                detail?.content.push(node);
-            }
-        }
-        attributes.details = details;
+        //转换数据
+        transformDetails(attributes);
 
         //写入文件
         await this.outputJson(filename, attributes);
@@ -96,3 +71,38 @@ export default new Processor({
         return jMeta;
     }
 });
+
+function transformDetails(attributes: JEntry) {
+    const originals = attributes.details as unknown as Child[];
+    if (!originals) {
+        return;
+    }
+
+    const details: EntryDetail[] = [];
+    let detail: EntryDetail;
+
+    for (let i = 0; i < originals.length; i++) {
+        const node = originals[i];
+
+        if (node.type === "element" && node.tag === "h2") {
+            if (node.children.length !== 1) {
+                continue;
+            }
+
+            const firstNode = node.children[0];
+            if (firstNode.type !== "text") {
+                continue;
+            }
+
+            detail = {
+                title: firstNode.value,
+                content: []
+            };
+            details.push(detail);
+        }
+        else {
+            detail?.content.push(node);
+        }
+    }
+    attributes.details = details;
+}
