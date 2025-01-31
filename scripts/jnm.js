51d0ccb4e9c24e4ba1608d42f94e5c8f965a0690
KazariEX <1364035137@qq.com>
2025-01-31 21:35:48 +0800

    chore: 自动获取并生成 Jnm.json

---
+++ scripts/jnm.js
@@ -0,0 +1,17 @@
+import fs from "fs-extra";
+import { resolve } from "pathe";
+
+const url = "https://namaemaker.net/wp-content/cache/autoptimize/autoptimize_single_4e118ae6b0f62cbfd6a1c0b41283e2c2.php";
+const res = await fetch(url);
+const text = await res.text();
+
+const Jnm = {};
+
+const reg = /randWordJnm(?<order>\d+(?:_kana)?)=new Array\((?<items>.*?)\)/g;
+for (const match of text.matchAll(reg)) {
+    const { order, items } = match.groups;
+    Jnm[order] = items.split(",").map((item) => item.slice(1, -1));
+}
+
+const outputPath = resolve(import.meta.dirname, "../public/json/Jnm.json");
+await fs.outputJson(outputPath, Jnm);
