4874e8ddf03dab769c9db654f4ed7acb9c429493
KazariEX <1364035137@qq.com>
2025-02-09 18:59:24 +0800

    refactor: 更换定时任务包

--- server/plugins/mongodump.ts
+++ server/plugins/mongodump.ts
@@ -1,32 +1,29 @@
 import { exec } from "node:child_process";
 import consola from "consola";
-import { CronJob } from "cron";
+import { Cron } from "croner";
 import { format } from "date-fns";
 
 export default defineNitroPlugin(() => {
     const config = useRuntimeConfig();
     const { options } = config.mongoose;
 
-    CronJob.from({
-        cronTime: "0 33 23 * * 7",
-        timeZone: "UTC+8",
-        onTick() {
-            //获取当前日期
-            const date = new Date();
+    const job = new Cron("0 33 23 * * 7", {
+        timezone: "Asia/Shanghai"
+    }, () => {
+        //获取当前日期
+        const date = new Date();
 
-            //备份路径
-            const path = r(`/server/backup/${format(date, "yyMMdd")}.archive`);
+        //备份路径
+        const path = r(`/server/backup/${format(date, "yyMMdd")}.archive`);
 
-            //运行命令
-            exec(`mongodump -u=${options.user} -p=${options.pass} -d=${options.dbName} --archive="${path}" --authenticationDatabase admin`, (err) => {
-                if (err) {
-                    consola.error(err);
-                }
-                else {
-                    consola.success("数据库备份成功！");
-                }
-            });
-        },
-        start: true
+        //运行命令
+        exec(`mongodump -u=${options.user} -p=${options.pass} -d=${options.dbName} --archive="${path}" --authenticationDatabase admin`, (err) => {
+            if (err) {
+                consola.error(err);
+            }
+            else {
+                consola.success("数据库备份成功！");
+            }
+        });
     });
 });
