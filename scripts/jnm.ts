9751039854b2026d08bf2ebbbe9f2220fc6278a4
KazariEX <zinkawaii@qq.com>
2025-08-17 20:23:45 +0800

    refactor: 从永久页面中读取 Jnm 数据以防止缓存失效

--- scripts/jnm.ts
+++ scripts/jnm.ts
@@ -1,17 +1,21 @@
 import { writeFile } from "node:fs/promises";
 import { resolve } from "pathe";
 
-const url = "https://namaemaker.net/wp-content/cache/autoptimize/autoptimize_single_4e118ae6b0f62cbfd6a1c0b41283e2c2.php";
+const url = await fetch("https://namaemaker.net/archives/japanese-name.html")
+    .then((res) => res.text())
+    .then((text) => text.match(/src="([^"]*autoptimize[^"]*.php)"/)![1]);
+
 const res = await fetch(url);
 const text = await res.text();
 
-const Jnm = {};
+const entityRE = /randWordJnm(?<order>\d+(?:_kana)?)=new Array\((?<items>.*?)\)/g;
 
-const reg = /randWordJnm(?<order>\d+(?:_kana)?)=new Array\((?<items>.*?)\)/g;
-for (const match of text.matchAll(reg)) {
-    const { order, items } = match.groups!;
-    Jnm[order] = items.split(",").map((item) => item.slice(1, -1));
-}
+const Jnm = Object.fromEntries(
+    [...text.matchAll(entityRE)].map((match) => {
+        const { order, items } = match.groups!;
+        return [order, items.split(",").map((item) => item.slice(1, -1))];
+    }),
+);
 
 const outputPath = resolve(import.meta.dirname, "../public/json/Jnm.json");
-await writeFile(outputPath, JSON.stringify(Jnm, null, 2));
+await writeFile(outputPath, JSON.stringify(Jnm));
