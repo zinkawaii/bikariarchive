b065aa46d58be4ea226f6adef5d764b5161fd0d3
KazariEX <zinkawaii@qq.com>
2026-06-06 08:43:56 +0800

    fix(article): 从存储中读取初始文章数据

--- modules/article/runtime/server.ts
+++ modules/article/runtime/server.ts
@@ -1,43 +1,45 @@
-import { readFile } from "node:fs/promises";
 import chokidar from "chokidar";
 import { definePlugin } from "nitro";
+import { useStorage } from "nitro/storage";
 import { basename, resolve } from "pathe";
 import { Article, enrichJArticle } from "#shared/utils/article";
 import { enrichJIntel } from "#shared/utils/entry";
 
 export default definePlugin(async (nitroApp) => {
-    const baseDir = resolve(".data/json");
+    const storage = useStorage("assets:data");
     const list = [
-        "article",
-        "artmap",
-        "intel",
-    ].map((name) => `${baseDir}/${name}.json`);
+        "json/article.json",
+        "json/artmap.json",
+        "json/intel.json",
+    ];
 
     for (const path of list) {
         await update(path);
     }
 
     if (import.meta.dev) {
-        const watcher = chokidar.watch(list).on("change", update);
+        const watcher = chokidar
+            .watch(list.map((path) => resolve(`.data/${path}`)))
+            .on("change", (path) => update(`json/${basename(path)}`));
         nitroApp.hooks.hook("close", () => watcher.close());
     }
-});
 
-async function update(path: string) {
-    const name = basename(path, ".json");
-    const data = await readFile(path, "utf-8").then(JSON.parse);
-    switch (name) {
-        case "article": {
-            enrichJArticle(data);
-            break;
-        }
-        case "artmap": {
-            Article.map = data;
-            break;
-        }
-        case "intel": {
-            enrichJIntel(data);
-            break;
+    async function update(path: string) {
+        const name = basename(path, ".json");
+        const data = await storage.getItem(path) as any;
+        switch (name) {
+            case "article": {
+                enrichJArticle(data);
+                break;
+            }
+            case "artmap": {
+                Article.map = data;
+                break;
+            }
+            case "intel": {
+                enrichJIntel(data);
+                break;
+            }
         }
     }
-}
+});
