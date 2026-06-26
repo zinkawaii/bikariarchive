8c679f55706ad44f1654afdfccb18fadf151ec47
KazariEX <zinkawaii@qq.com>
2026-06-26 23:47:27 +0800

    refactor: 使用 `mrmime` 代替 `video-extensions`

--- packages/article/src/remark/handlers/image.ts
+++ packages/article/src/remark/handlers/image.ts
@@ -1,6 +1,6 @@
+import { lookup } from "mrmime";
 import { extname } from "pathe";
 import { parsePath } from "ufo";
-import videoExtensions from "video-extensions";
 import type { Element } from "hast";
 import type { Image } from "mdast";
 import type { Handler } from "mdast-util-to-hast";
@@ -15,7 +15,7 @@ export default <Handler> function(state, node: Image) {
   const path = parsePath(node.url).pathname;
   const ext = extname(path).slice(1).toLowerCase();
 
-  const result: Element = videoExtensions.includes(ext)
+  const result: Element = lookup(ext)?.startsWith("video/")
     ? {
       type: "element",
       tagName: "mb-video",
